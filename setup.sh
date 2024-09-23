92cdccc8a2366ad76471d59ed83b9ad67a69a9b5
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-23 12:53:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt update
 add-apt-repository ppa:deadsnakes/ppa
 apt update
-apt install python3.10 python3-pipenv
+apt install python3.10 pipenv
 git clone https://github.com/Urstar3/Ctfd_fork1
 mv  Ctfd_fork1 /ctf
 cd /ctf
