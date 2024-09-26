f91693b70723bbe34d6daf45a1d0fb846bf045c7
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-26 10:00:03 +0800

    add pipenv.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 # bin/bash
-apt update
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install python3.10 pipenv
+sudo apt update
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.10 pipenv nginx
 git clone https://github.com/CTFd/CTFd.git
 mv  CTFd /ctf
+cp pipenv.sh /ctf
 cd /ctf
+sudo mkdir -p /var/log/ctf/logs
+sudo chown ctfd:ctfd /var/log/ctf/logs
 pipenv install --python 3.10
 pipenv shell
 
