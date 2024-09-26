3f72bc3282fd303556e0c5b6352e679349b8e013
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-26 10:06:07 +0800

    added precompiled cffi and pybluemonday

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ sudo apt update
 sudo apt install python3.10 pipenv nginx
 git clone https://github.com/CTFd/CTFd.git
 mv  CTFd /ctf
+cp cffi-1.18.0.dev0-cp312-cp312-linux_x86_64.whl /ctf
+cp pybluemonday-0.0.12-cp312-cp312-linux_x86_64.whl /ctf
 cp pipenv.sh /ctf
 cd /ctf
 sudo mkdir -p /var/log/ctf/logs
