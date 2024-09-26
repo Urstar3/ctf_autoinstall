be90f11194b92b158c1e5cbfc47c15e107ee37b4
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-26 09:39:41 +0800

    Update setup.sh
    simplified autoinstall

--- setup.sh
+++ setup.sh
@@ -3,14 +3,11 @@ apt update
 add-apt-repository ppa:deadsnakes/ppa
 apt update
 apt install python3.10 pipenv
-git clone https://github.com/Urstar3/Ctfd_fork1
-mv  Ctfd_fork1 /ctf
+git clone https://github.com/CTFd/CTFd.git
+mv  CTFd /ctf
 cd /ctf
+pipenv install --python 3.10
 pipenv shell
-pip install -r requirements.txt
-pip install cffi.whl 
-pip install pybluemonday.whl
-gunicorn --bind 0.0.0.0:9800 'CTFd:create_app()'
 
 
 
