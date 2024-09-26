460d1f9d144d4225e4104fea6d51b02c6f9bdf7b
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-26 16:50:32 +0800

    added gunicorn.service add ctfd config and update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ sudo apt update
 sudo apt install python3.10 pipenv nginx
 git clone https://github.com/CTFd/CTFd.git
 mv  CTFd /ctf
+sudo cp gunicorn.service /etc/systemd/system/
+sudo cp ctfd /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/default/ /etc/nginx/sites-enabled
+rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 cp cffi-1.18.0.dev0-cp312-cp312-linux_x86_64.whl /ctf
 cp pybluemonday-0.0.12-cp312-cp312-linux_x86_64.whl /ctf
 cp pipenv.sh /ctf
