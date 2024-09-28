cacb659ecdc90e41d45418065e29f6cbbffa2e07
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-09-28 16:28:54 +0800

    added readmie md

--- pipenv.sh
+++ pipenv.sh
@@ -1,6 +1,4 @@
 # bin/bash
 pip install -U setuptools
 pip install -r requirements.txt
-gunicorn --bind 0.0.0.0:8000 'CTFd:create_app()'
-path = pipenv --venv
 
--- setup.sh
+++ setup.sh
@@ -2,19 +2,20 @@
 sudo apt update
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.10 pipenv nginx
+sudo apt install python3.10 pipenv nginx mariadb-server redis-server
 git clone https://github.com/CTFd/CTFd.git
-mv  CTFd /ctf
+sudo mv  CTFd /ctf
+sudo chwon ctf:ctf /ctf
 sudo cp gunicorn.service /etc/systemd/system/
 sudo cp ctfd /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/default/ /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/ctfd /etc/nginx/sites-enabled
 rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 cp cffi-1.18.0.dev0-cp312-cp312-linux_x86_64.whl /ctf
 cp pybluemonday-0.0.12-cp312-cp312-linux_x86_64.whl /ctf
 cp pipenv.sh /ctf
 cd /ctf
-sudo mkdir -p /var/log/ctf/logs
-sudo chown ctfd:ctfd /var/log/ctf/logs
+sudo mkdir -p /var/log/ctfd/logs
+sudo chown ctf:ctf /var/log/ctfd/logs
 pipenv install --python 3.10
 pipenv shell
 
